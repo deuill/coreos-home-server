3254ead3a7bbb3c3b8a90036d9b138c855703a5d
Alex Palaistras <alex@deuill.org>
2021-06-27 12:52:47 +0100

    Simplify systemd services, use volumes
    This commit represents a large amount of work toward moving services to
    a more standard approach to storing data, and a simplification in how
    networks are managed.

--- config/service/prosody/service/config/virtualhost.cfg.lua
+++ config/service/prosody/service/config/virtualhost.cfg.lua
@@ -1,14 +1,14 @@
 VirtualHost(os.getenv("PROSODY_HOST"))
   http_host = os.getenv("PROSODY_HOST_EXTERNAL")
   http_external_url = "https://" .. os.getenv("PROSODY_HOST_EXTERNAL") .. "/"
-  certificate = "/etc/ssl/private/" .. os.getenv("PROSODY_HOST") .. "/tls.crt"
+  certificate = "/etc/ssl/private/certificates/" .. os.getenv("PROSODY_HOST") .. ".crt"
   authentication = "imap"
   auth_append_host = true
 
 Component(os.getenv("PROSODY_HOST_EXTERNAL")) "muc"
   modules_enabled = {"muc_mam", "vcard_muc"}
   name = "The " .. os.getenv("PROSODY_HOST") .. " chat-room server"
-  certificate = "/etc/ssl/private/" .. os.getenv("PROSODY_HOST_EXTERNAL") .. "/tls.crt"
+  certificate = "/etc/ssl/private/certificates/" .. os.getenv("PROSODY_HOST_EXTERNAL") .. ".crt"
   restrict_room_creation = "local"
   max_history_messages = 100
 
