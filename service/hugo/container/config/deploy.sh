0493a7fbfaa8f17174cf055de8ccee6de8bc2048
Alex Palaistras <alex@deuill.org>
2024-01-02 16:11:26 +0000

    hugo: Move to multi-stage container build
    As with other container definitions, this helps make layers more
    cacheable both between containers and in version updates.

--- service/hugo/container/config/deploy.sh
+++ service/hugo/container/config/deploy.sh
@@ -28,4 +28,4 @@ else
     cd "$GIT_DIR_NAME"
 fi
 
-hugo --logLevel info --minify --destination "/build/${GIT_DIR_NAME}"
+exec /usr/bin/hugo --logLevel info --minify --destination "/build/${GIT_DIR_NAME}"
